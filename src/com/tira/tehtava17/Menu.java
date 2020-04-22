3eec169d9065e91ddda1aa091b7ba9f249649b2f
Katri Rasio <katri.rasio@metropolia.fi>
2020-04-22 16:02:07 +0300

    inOrder

---
+++ src/com/tira/tehtava17/Menu.java
@@ -0,0 +1,77 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package com.tira.tehtava17;
+
+import java.util.Iterator;
+import java.util.TreeSet;
+
+/**
+ *
+ * @author kamaj
+ */
+public class Menu {
+    //main alkaa-----------------------------------------------------------------------------
+    public static void main(String[] args) {
+        printMenu();
+
+    }
+    //main loppuu --------------------------------------------------------------------------
+//printMenu alkaa------------------------------------------------------------------
+    private static void printMenu() {
+        Integer select;
+        TreeSet tree = new TreeSet<>();
+//        String[] sList = {"david", "bo", "coco", "angela", "emil", "fabio", "gabby"}
+        Integer[] list = {4, 32, 56, 21, 56778, 224, 1, -88, 0, 282, 282};
+
+//        String[] sList = {"k", "n", "e", "f", "g", "r", "s", "a", "m", "t",  "o", "l", "u", "v"};
+        for (Integer i : list) {
+            tree.add(i);
+        }
+
+        Integer data;
+        do {
+            System.out.print("\n\t\t1. Lisää avain.");
+            System.out.print("\t\t\t2. Etsi avaimella.");
+            System.out.print("\t\t\t3. Käy puu läpi esijärjestyksessä.");
+            System.out.print("\t\t\t4. Poista avaimella.");
+            System.out.println("\t\t\t5. lopetus ");
+            select = Lue.kluku();
+            switch (select) {
+                case 1:
+                    System.out.println("Anna uusi avain (merkkijono)");
+                    data = Lue.kluku();
+                    tree.add(data);
+                    break;
+                case 2:
+                    System.out.println("Anna etsittävä avain (merkkijono)");
+                    data = Lue.kluku();
+                    if (tree.contains(data)){
+                        System.out.println("Avain löytyi.");
+                    }
+                    else {
+                        System.out.println("Avainta ei löytynyt.");
+                    }
+                    break;
+                case 3:
+                    Iterator<Integer> i = tree.iterator();
+                    while(i.hasNext()) {
+                        System.out.println(i.next() + " ");
+                    }
+                    System.out.println();
+                    break;
+                case 4:
+                    data = Lue.kluku();
+                    tree.remove(data);
+                    break;
+                case 5:
+                    break;
+            }
+        }
+        while (select != 5);
+    }
+//printMenu loppuu ----------------------------------------------------------------
+}
+

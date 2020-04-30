f566e7c1f36b3f4b9cb3b4ccdfc38f62fb3e8891
Katri Rasio <katri.rasio@metropolia.fi>
2020-04-30 12:23:42 +0300

    fix compare so that it works when difference is <1

--- src/com/tira/tehtava19202122/BalanceComparator.java
+++ src/com/tira/tehtava19202122/BalanceComparator.java
@@ -7,12 +7,11 @@ public class BalanceComparator implements Comparator<Object> {
     public int compare(Object o1, Object o2) {
         TrafficCard t1 = (TrafficCard)o1;
         TrafficCard t2 = (TrafficCard)o2;
-//        if (t1.mBalance == t2.mBalance) {
-//            return 0;
-//        } else if (t1.mBalance > t2.mBalance) {
-//            return 1;
-//        }
-//        return -1;
-        return (int)(t1.mBalance - t2.mBalance);
+        if (t1.mBalance == t2.mBalance) {
+            return 0;
+        } else if (t1.mBalance > t2.mBalance) {
+            return 1;
+        }
+        return -1;
     }
 }
